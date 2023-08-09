7fe4f8a60d6fc5f4dd093be359b78d6193af988e
p0chitaa <retr0.spekktion@gmail.com>
2023-08-09 13:56:21 -0400

    adds declarativeRendering vue page

--- vue-tutorial/src/DeclarativeRendering.vue
+++ vue-tutorial/src/DeclarativeRendering.vue
@@ -1,9 +1,32 @@
 <script setup>
 import { ref } from 'vue'
+import { reactive } from 'vue'
 
-//logic
+/*
+The core feature of Vue is declarative rendering: using a template syntax 
+that extends HTML, we can describe how the HTML should look like based on 
+JavaScript state. When the state changes, the HTML updates automatically.
+*/
+
+/*
+States that can trigger upadtes when changed are considered REACTIVE.
+We can declare reactive state using Vue's `reactive()` API. Objects
+created from `reactive()` are JavaScript Proxies that work just like
+normal objects.
+*/
+
+const counter = reactive({ count: 0 })
+
+console.log(counter.count) // 0
+counter.count++
+
+const message = ref('Hello World!')
+
+console.log(message.value) // "Hello World!""
+message.value = 'Changed'
 </script>
 
 <template>
-    <h1>Make me dynamic!!!</h1>
+    <h1>{{ message }}</h1>
+    <p>count is: {{ counter.count }}</p>
 </template>
